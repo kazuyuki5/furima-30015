cfeb900715dc4456f2e7ada2695b68c02a97594f
kazuyuki5 <kazuyuki2957@gmail.com>
2020-11-16 11:24:18 +0900

    テストコード、Factorybotの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,10 +4,10 @@ FactoryBot.define do
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    kanji_family_name     {Faker::Name.initials(number: 2)}
-    kanji_first_name      {Faker::Name.initials(number: 2)}
-    kana_family_name      {Faker::Name.initials(number: 2)}
-    kana_first_name       {Faker::Name.initials(number: 2)}
+    kanji_family_name     {"山田"}
+    kanji_first_name      {"太郎"}
+    kana_family_name      {"ヤマダ"}
+    kana_first_name       {"タロウ"}
     birthday              {"2000-01-01"}
   end
 end
