ecc3dd8e747e079cc2ce79b2ae827266f7b35d41
kazuyuki5 <kazuyuki2957@gmail.com>
2020-11-16 20:53:40 +0900

    areaモデルの生成

--- db/migrate/20201116100949_create_items.rb
+++ db/migrate/20201116100949_create_items.rb
@@ -6,6 +6,8 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer    :category_id     , null: false
       t.integer    :condition_id    , null: false
       t.integer    :delivery_fee_id , null: false
+      t.integer    :area_id         , null: false
+      t.
       t.timestamps
     end
   end
