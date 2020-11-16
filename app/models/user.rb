2a361c575facf61a499910f2c9d2ced0fcde1912
kazuyuki5 <kazuyuki2957@gmail.com>
2020-11-16 16:23:10 +0900

    正規表現を変数に代入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         KANJI_REGEX = /\A[ぁ-んァ-ン一-龥]/
+         KANA_REGEX = /\A[ァ-ヶー－]+\z/
         with_options presence: true do
          validates :nickname
-         validates :kanji_family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-         validates :kanji_first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-         validates :kana_family_name, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-         validates :kana_first_name, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+         validates :kanji_family_name, format: { with: KANJI_REGEX, message: "is invalid. Input full-width characters."}
+         validates :kanji_first_name, format: { with: KANJI_REGEX, message: "is invalid. Input full-width characters."}
+         validates :kana_family_name, format: { with: KANA_REGEX, message: "is invalid. Input full-width katakana characters."}
+         validates :kana_first_name, format: { with: KANA_REGEX, message: "is invalid. Input full-width katakana characters."}
          validates :birthday
         end
 end
