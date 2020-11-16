1c572ebfd2fd8af1f3ac1f68054d5d48c2537f41
kazuyuki5 <kazuyuki2957@gmail.com>
2020-11-16 20:40:12 +0900

    delivery_feeモデルの生成

--- db/migrate/20201116100949_create_items.rb
+++ db/migrate/20201116100949_create_items.rb
@@ -1,11 +1,11 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string     :name         , null: false
-      t.text       :explanation  , null: false
-      t.integer    :category_id  , null: false
-      t.integer    :condition_id , null: false
-      t.
+      t.string     :name            , null: false
+      t.text       :explanation     , null: false
+      t.integer    :category_id     , null: false
+      t.integer    :condition_id    , null: false
+      t.integer    :delivery_fee_id , null: false
       t.timestamps
     end
   end
