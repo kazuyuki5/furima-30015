de13e22a5aae9be1a8b6ac833dc36ae949160e43
kazuyuki5 <kazuyuki2957@gmail.com>
2020-11-17 12:10:31 +0900

    出品ページへのルートパスを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,8 +8,9 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
-  validates :image, :name, :explanation, :price format: { with: 300 =< 9999999, \d }
-   with_option numericality: { other_than: 1 } do
+  validates :image, :name, :explanation
+  validates :price
+   with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :condition_id
     validates :delivery_fee_id
