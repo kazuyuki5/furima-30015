151bb675f5b5db52d2d04403f939464f6ccce2cd
kazuyuki5 <kazuyuki2957@gmail.com>
2020-11-18 10:54:36 +0900

    test用画像の導入とpriceの半角英数のバリデーションの設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name {Faker::Lorem.sentence}
+    explanation {'かっこいい'}
+    category_id {'レディース'}
+    condition_id {'新品、未使用'}
+    delivery_fee_id {'着払い（購入者負担）'}
+    area_id {'北海道'}
+    delivery_day_id {'1~2日で発送' }
+    price  {500}
+    association :user 
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @tweet = FactoryBot.build(:item)
+  end
+
+  describe '商品の保存' do
+    context "商品が保存できる場合" do
+      it "画像、商品名、商品説明、状態、カテゴリー、配送料の負担、発送元の地域、発送までの日数、価格があれば商品出品は保存される" do
+        expect(@item).to be_valid
+      end
+      it "テキストのみあればツイートは保存される" do
+      end
+    end
+    context "商品が保存できない場合" do
+      it "テキストがないとツイートは保存できない" do
+      end     
+      it "ユーザーが紐付いていないとツイートは保存できない" do
+      end
+    end
+  end
 end
