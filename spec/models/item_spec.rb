f3fe9bc1ea0911f25976fa9531ef6853b3045ce4
kazuyuki5 <kazuyuki2957@gmail.com>
2020-11-20 06:07:59 +0900

    価格の範囲テストコードを２つに分けた。

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -52,8 +52,12 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
-      it '価格の範囲が￥300〜￥9,999,999以外だと保存できないこと' do
-        @item.price = 1
+      it '価格の範囲が￥299以下だと保存できないこと' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it '価格の範囲が￥10,000,000以上だと保存できないこと' do
         @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
