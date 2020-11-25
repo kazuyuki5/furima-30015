84c21870ecf0473275f49614cd6cce0a749e14ae
kazuyuki5 <kazuyuki2957@gmail.com>
2020-11-25 20:23:04 +0900

    購入ページから情報の保存のためのパスを指定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,12 +6,9 @@ class OrdersController < ApplicationController
 
 
   def create
-
+  binding.pry
   end
 
   private
-  # 全てのストロングパラメーターを1つに統合
- def donation_params
-  params.require(:user_donation).permit(:name, :name_reading, :nickname, :postal_code, :prefecture, :city, :house_number, :building_name, :price)
- end
+  
 end
